7396c57d12b518f70aa4b4280e8b7b12f1fdb8b6
lazharkarem <karem.lazhar@gmail.com>
2024-09-16 04:30:11 +0100

    database.php config

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'tn360'),
+            'username' => env('DB_USERNAME', 'tn360'),
+            'password' => env('DB_PASSWORD', 'Kr,Lr}&~GXa?~7|Y'),
+            'unix_socket' => env('DB_SOCKET', '/cloudsql/tn-421315:europe-west1:tn360'),
+
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -61,7 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-        ],
+    ],
 
         'pgsql' => [
             'driver' => 'pgsql',
